1b1e7fb9e899d66d3fe40d6f947c91e74b3f0b95
Hariharan V <64734193+hariharanvvu@users.noreply.github.com>
2021-06-05 00:18:56 +0530

    web server learning codes
    Adding files for first time

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.send('Hello Express!')
+})
+
+app.get('/help', (req, res) => {
+    res.send('Help page')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About page')
+})
+
+app.get('/weather', (req, res) => {
+    res.send('Weather page')
+})
+
+app.listen('8080',(res) => {
+    console.log('Server is up and running in port 8080')
+})
