09ac1695164a312c0861cff82d6dc51f696c7512
Hariharan V <64734193+hariharanvvu@users.noreply.github.com>
2021-06-05 15:43:45 +0530

    Updated with hbs usage

--- src/app.js
+++ src/app.js
@@ -1,25 +1,39 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define path for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+// Setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+
+// Setup static Directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-    res.send('Hello Express!')
-})
-
-app.get('/help', (req, res) => {
-    res.send('Help page')
+    res.render('index',{
+        title: 'Weather',
+        name: 'Hariharan'
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.render('about',{
+        title: 'About',
+        name: 'Hariharan'
+    })
 })
 
-app.get('/weather', (req, res) => {
-    res.send('Weather page')
+app.get('/help', (req, res) => {
+    res.render('help',{
+        title: 'Help',
+        helpText: 'This is some helpful text'
+    })
 })
 
 app.listen('8080',(res) => {
